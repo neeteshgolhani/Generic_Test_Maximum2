cea6968c920d09cd73e0467468008c3cd40b6e60
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-04-27 00:47:11 +0530

    Added : Given 3 Integers find the maximum Using generic class

--- src/com/bridgelabz/FindMaxValueUsingGeneric.java
+++ src/com/bridgelabz/FindMaxValueUsingGeneric.java
@@ -1,21 +1,35 @@
 package com.bridgelabz;
-/*The key point to note here is that the <T extends Comparable<T>> syntax specifies
- that the method only works with types T that implement the Comparable interface.
- This is necessary because we use the compareTo method to compare values within the method.
+/* Sure! Here's a refactored version of the code to create a generic class that
+takes in three variables of generic type and ensures that the generic type extends Comparable.
+I've also added a parameter constructor and a testMaximum method to internally call the
+static findMaxValue method and return the maximum value of the three variables.
  */
-public class FindMaxValueUsingGeneric {
     // This method takes in three values of type T (which must be comparable),
     // and returns the maximum value of the three.
-    public static <T extends Comparable<T>> T findMaxValue(T a1, T a2, T a3){
-        // Start by assuming that a1 is the max.
-        T max = a1;
-        // Check if a2 is greater than the current max.
-        if (a2.compareTo(max) > 0)
-            max = a2;
-        // Check if a3 is greater than the current max.
-        if (a3.compareTo(max) > 0)
-            max = a3;
-        // Return the maximum value.
-        return max;
+    public class FindMaxValueUsingGeneric<T extends Comparable<T>> {
+        private T a1;
+        private T a2;
+        private T a3;
+
+        // Parameter constructor to initialize the three variables
+        public FindMaxValueUsingGeneric(T a1, T a2, T a3) {
+            this.a1 = a1;
+            this.a2 = a2;
+            this.a3 = a3;
+        }
+
+        // Method to find the maximum value of the three variables
+        public T testMaximum() {
+            return FindMaxValueUsingGeneric.findMaxValue(a1, a2, a3);
+        }
+
+        // Static method to find the maximum value of three generic variables
+        public static <T extends Comparable<T>> T findMaxValue(T a1, T a2, T a3) {
+            T max = a1;
+            if (a2.compareTo(max) > 0)
+                max = a2;
+            if (a3.compareTo(max) > 0)
+                max = a3;
+            return max;
+        }
     }
-}
--- src/com/bridgelabz/GenericMain.java
+++ src/com/bridgelabz/GenericMain.java
@@ -1,24 +1,26 @@
 package com.bridgelabz;
 import static com.bridgelabz.FindMaxValueUsingGeneric.findMaxValue;
-/*In this code, the findMaxValue method is being called with different types
- of inputs (integers, floats, and strings), but it works for all of them because it uses generics.
-  The <T extends Comparable<T>> syntax allows the method to work with any
-  type T that implements the Comparable interface, which includes all of these types.
-   The System.out.println statements
- print out the maximum value returned by the findMaxValue method for each input type.
+/*In this example, the FindMaxValueUsingGeneric class is being used to find
+the maximum value of three generic variables (integers, floats, and strings).
+The testMaximum method is called on each instance of the class, which internally
+calls the findMaxValue static method and returns the maximum value. The System.out.println statements
+ print out the maximum value returned by the testMaximum method for each input type.
 * */
 public class GenericMain {
-     public static void main(String[] args) {
-     // Print out a welcome message.
-        System.out.println("Welcome to Find Maximum Problem Using Generics.");
+    public static void main(String[] args) {
+        // Create a new instance of the FindMaxValueUsingGeneric class with integer inputs
+        FindMaxValueUsingGeneric<Integer> integerMax = new FindMaxValueUsingGeneric<>(20, 12, 15);
+        // Call the testMaximum method and print out the maximum value
+        System.out.println("The maximum value between the three Integers is : " + integerMax.testMaximum());
 
-     // Call findMaxValue with three integer inputs and print out the maximum value.
-        System.out.println("The maximum value between the three Integers is : "+findMaxValue(20, 12, 15));
+        // Create a new instance of the FindMaxValueUsingGeneric class with float inputs
+        FindMaxValueUsingGeneric<Float> floatMax = new FindMaxValueUsingGeneric<>(20.3f, 22.1f, 15.9f);
+        // Call the testMaximum method and print out the maximum value
+        System.out.println("The maximum value between the three Floats is : " + floatMax.testMaximum());
 
-     // Call findMaxValue with three float inputs and print out the maximum value.
-        System.out.println("The maximum value between the three Floats is : "+findMaxValue(20.3f, 22.1f, 15.9f));
-
-     // Call findMaxValue with three string inputs and print out the maximum value.
-        System.out.println("The maximum value between the three Strings is : "+findMaxValue("Apple", "Peach", "Banana"));
-}
+        // Create a new instance of the FindMaxValueUsingGeneric class with string inputs
+        FindMaxValueUsingGeneric<String> stringMax = new FindMaxValueUsingGeneric<>("Apple", "Peach", "Banana");
+        // Call the testMaximum method and print out the maximum value
+        System.out.println("The maximum value between the three Strings is : " + stringMax.testMaximum());
+    }
 }
